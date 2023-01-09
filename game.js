b7f0fcaabf95aa119974167d852194845efa475b
Brianne Bourassa <shinybri1011@gmail.com>
2023-01-08 19:10:09 -0700

    Initial commit

---
+++ game.js
@@ -0,0 +1,8 @@
+class Game{
+    constructor(){
+        this.player1 = new Player('trainer1');
+        this.player2 = new Player('trainer2');
+        this.turn = trainer1;
+
+    }
+};
---
+++ player.js
@@ -0,0 +1,7 @@
+class Player {
+    constructor(name, token){
+        this.name = name;
+        this.token = token;
+        this.wins = 0
+    }
+};
