36582f10dda78f4727532d8a043d95926ce0b40f
Brianne Bourassa <shinybri1011@gmail.com>
2023-01-11 11:23:24 -0700

    made buttons in html and begin working in main.js

--- game.js
+++ game.js
@@ -2,7 +2,33 @@ class Game{
     constructor(){
         this.player1 = new Player('trainer1');
         this.player2 = new Player('trainer2');
-        this.turn = trainer1;
+        // this.turn = trainer1;
 
     }
+
+
+
+
+
+
+   
+
+
+
+ 
+    // way to keep track of the data for the game board (which pkmn out? OR JUST WHICH PKMN)
+    // way to keep track of the selected game type (3 or 5)
+    // way to check the Game's board data for win condition (type weakness etc)
+    // a way to detect when a game is a draw
+    // a way to reset the board and start new game
+
 };
+
+// var water = {
+//     name: 'Blastoise',
+//     img: ['/assets/blastoise-L.gif', '/assets/blastoise-R.gif']
+// }
+// var fire = {
+//     name: 'Charizard',
+//     img: ['/assets/charizard-L.gif', '/assets/charizard-R.gif']
+// }
--- main.js
+++ main.js
@@ -1,13 +1,70 @@
 
+// WIN CONDITIONS
+var water = {
+  name: 'Blastoise',
+  img: ['/assets/blastoise-L.gif', '/assets/blastoise-R.gif']
+}
+var fire = {
+  name: 'Charizard',
+  img: ['/assets/charizard-L.gif', '/assets/charizard-R.gif']
+}
 
 
-// function on() {
-//     document.getElementById("choose-pkmn").style.display = "block";
-//   }
+
+var fireIcon3 = document.querySelector('#fire-icon-3');
+var waterIcon3 = document.querySelector('#water-icon-3');
+var grassIcon3 = document.querySelector('#grass-icon-3');
+
+var fireIcon5 = document.querySelector('#fire-icon-5');
+var waterIcon5 = document.querySelector('#water-icon-5');
+var grassIcon5 = document.querySelector('#grass-icon-5');
+var electricIcon5 = document.querySelector('#electri-icon-5');
+var groundIcon5 = document.querySelector('#ground-icon-5');
+
+
+var mainGameBoard = document.querySelector('#main-game');
+var overlay = document.querySelector('.overlay');
+var winOrLose = document.querySelector('#win-lose-display');
+var leftSide = document.querySelector('#left-side');
+var rightSide = document.querySelector('#right-side');
+
+fireIcon3.addEventListener('click', choosePokemon);
+
+
+
+function choosePokemon(){
+  const game = new Game()
+  game.player1.takeTurn();
+
+  // conditional BUTTONS
+
+  leftSide.innerHTML += 
+  `<img class="pokemon-L" src="assets/charizard-L.gif"/>`
+
+
   
-//   function off() {
-//     document.getElementById("choose-pkmn").style.display = "none";
-//   }
+  computerRandom3()
+}
 
 
-//   https://www.youtube.com/watch?v=Shmi49uR1BY
+function computerRandom3(){
+  var rightSidePkmn = ['/assets/blastoise-R.gif', '/assets/charizard-R.gif', '/assets/venusaur-R.gif']
+  var pkmn = rightSidePkmn[Math.floor(Math.random()*rightSidePkmn.length)]
+  rightSide.innerHTML += 
+  `<img src="${pkmn}"/>`
+}
+
+
+
+
+// connect to challenge mode section??????
+function computerRandom5(){
+  var rightSidePkmn = ['/assets/blastoise-R.gif', '/assets/charizard-R.gif', '/assets/venusaur-R.gif', '/assets/raichu-R.gif', '/assets/sandslash-R.gif']
+  var pkmn = rightSidePkmn[Math.floor(Math.random()*rightSidePkmn.length)]
+  rightSide.innerHTML += 
+  `<img src="${pkmn}"/>`
+}
+
+
+
+
