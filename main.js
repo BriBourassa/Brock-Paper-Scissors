63ea57893db076801eb8b87b4e38b0c060e4ba28
Brianne Bourassa <shinybri1011@gmail.com>
2023-01-10 17:11:02 -0700

    Add assets and style overlay

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+// function on() {
+//     document.getElementById("choose-pkmn").style.display = "block";
+//   }
+  
+//   function off() {
+//     document.getElementById("choose-pkmn").style.display = "none";
+//   }
+
+
+//   https://www.youtube.com/watch?v=Shmi49uR1BY
